2a3303c40c59910ed57695da47645dceea5ade7b
96neko1st <seed.kira07@gmail.com>
2017-12-22 01:03:16 +0900

    add:機能追加 ボタンのレイアウト、スロットの基本的機能を追加

--- www/js/main.js
+++ www/js/main.js
@@ -7,19 +7,67 @@
         'bell.png',
         'cherry.png'
         ];
-        
+      
+    var timers = []; 
+    var stopCount = 0;
+    
+    initPanel();
+    
+    //パネル画像をランダムに切り替える
     function runSlot(n){
-        setTimeout(function(){
+        timers[n] = setTimeout(function(){
             panels[n].children[0].src = 
             './img/' + cards[Math.floor(Math.random() * cards.length)];
             runSlot(n);
         }, 50);
     }
     
+    //パネルの初期化
+    function initPanel(){
+        for(var i = 0; i < panels.length; i++){
+            panels[i].children[1].addEventListener('click', function(){
+                if(this.className.indexOf("inactive") !== -1){
+                    return;
+                }
+                clearTimeout(timers[this.dataset.index]);
+                stopCount++;
+                this.className = "stop inactive";
+                if(stopCount === panels.length){
+                    stopCount = 0;
+                    spin.className = "";
+                    checkResults();
+                }
+            });
+        }
+    }
+    
+    //一致していないパネルがあると色を薄くする
+    function checkResults(){
+        var panelLeft   = panels[0].children[0];
+        var panelCenter = panels[1].children[0];
+        var panelRight  = panels[2].children[0];
+        
+        if(panelLeft.src !== panelCenter.src && panelLeft.src !== panelRight.src){
+            panelLeft.className = 'unmatched';
+        }
+        if(panelCenter.src !== panelLeft.src && panelCenter.src !== panelRight.src){
+            panelCenter.className = 'unmatched';
+        }
+        if(panelRight.src !== panelCenter.src && panelRight.src !== panelLeft.src){
+            panelRight.className = 'unmatched';
+        }
+    }
+    
+    //spinボタンを押すと回転が始まる。
     spin.addEventListener('click', function(){
-        var i;
-        for(i = 0; i < panels.length; i++){
+        if(this.className.indexOf("inactive") !==-1){
+            return;
+        }
+        this.className = "inactive";
+        for(var i = 0; i < panels.length; i++){
             runSlot(i);
+            panels[i].children[0].className = "";
+            panels[i].children[1].className = "stop"
         }
     });
 })();
